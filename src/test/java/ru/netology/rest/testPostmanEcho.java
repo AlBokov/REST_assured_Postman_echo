6b22f4a8b2abbecc6eb35a68ef481b9cd63aed53
Alexander Bokov <alexander.bokov30@yandex.ru>
2024-05-31 17:14:32 +0300

    start project

---
+++ src/test/java/ru/netology/rest/testPostmanEcho.java
@@ -0,0 +1,22 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+
+
+public class testPostmanEcho {
+    @Test
+    void shouldReturnSendData() {
+        String text = "Hello world";
+        given()
+                .baseUri("https://postman-echo.com")
+                .body(text)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo("Hello"));
+
+    }
+
+}
